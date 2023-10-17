e3c2e791762a0877206e134d882dfbff440269ec
EduarMon <emondragono001@alumno.uaemex.mx>
2023-10-17 10:20:03 -0600

    El cuadrado

---
+++ Cuadrado.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Cuadrado {
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para la entrada de usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar al usuario que ingrese los lados del cuadrado
+        System.out.print("Ingresa la longitud del lado: ");
+        double lado = scanner.nextDouble();
+
+        // Calcular el área y el perímetro del rectángulo
+        double area = lado * lado;
+        double perimetro = 4 * lado;
+
+        // Mostrar los resultados
+        System.out.println("Cuadrado:");
+        System.out.println("Área: " + area);
+        System.out.println("Perímetro: " + perimetro);
+
+        // Cerrar el objeto Scanner
+        scanner.close();
+    }
+}
